16cec321f9b8f580ff60579ac04b46b928af9faf
yshls <shluxnsal01@gmail.com>
2025-10-17 23:22:45 +0900

    feat(Input): handle empty string a input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ class App {
       const input = await Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n"'
       );
+
+      // 2. 초기 예외 처리
+      if (input === '') {
+        Console.print('결과 : 0');
+        return;
+      }
     } catch (error) {}
   }
 }
