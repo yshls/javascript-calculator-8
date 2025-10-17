aaa2edb88b4cd316f077fd78d3da60e3777a61e3
yshls <shluxnsal01@gmail.com>
2025-10-17 23:21:42 +0900

    feat(Input): receive user  string input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Console } from '@woowacourse/mission-utils';
 class App {
-  async run() {}
+  async run() {
+    // 1. 사용자에게 입력값 받기
+    try {
+      const input = await Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n"'
+      );
+    } catch (error) {}
+  }
 }
 
 export default App;
